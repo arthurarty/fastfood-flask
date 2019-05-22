981b5822413060977be93bbc2093cbb34e10d5aa
arthurarty <arthur.nangai@gmail.com>
2019-05-22 19:09:29 +0300

    feat(docker) - Add postgres.
    - Add docker-compose file.
    [Finishes #166182848]

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,5 @@ FROM python:3.6
 WORKDIR /FASTFOOD-FLASK
 COPY . /FASTFOOD-FLASK
 RUN pip install -r requirements.txt
-EXPOSE 5000
 ENV FLASK_APP=flaskr
 ENV FLASK_ENV=development
-CMD ["flask", "run", "--host=0.0.0.0"]
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -9,7 +9,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.getenv('SECRET_KEY'),
-        SQLALCHEMY_DATABASE_URI='postgres://postgres:qwe@localhost:5432/fastfood',
+        SQLALCHEMY_DATABASE_URI=os.getenv('DB_URL'),
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
     from .model import db
