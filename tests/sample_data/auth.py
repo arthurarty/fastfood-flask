641587c9e0d233d5c48b348af9c83bdf78d90c05
Arthur Nangai <arthurarty@users.noreply.github.com>
2019-07-21 18:57:29 +0300

    chore(auth) - Add regex expression. (#13)

--- tests/sample_data/auth.py
+++ tests/sample_data/auth.py
@@ -1,14 +1,14 @@
 user_1 = {
-    'email': 'test@example.com',
+    'email': 'testyou@exa.com',
     'password': 'example'
 }
 
 admin_user = {
-    'email': 'admin@example.com',
+    'email': 'admin.example@exa.com',
     'password': 'Password54'
 }
 
 wrong_user_password = {
-    'email': 'test@example.com',
+    'email': 'testyou@exa.com',
     'password': 'example12'
 }
